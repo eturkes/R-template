32f7b867988342b7453431b0e68fcee636c66c2d
Emir Turkes <eturkes@bu.edu>
2020-03-31 09:38:00 -0400

    Fix path names

--- singularity.sh
+++ singularity.sh
@@ -11,14 +11,14 @@ singularity pull -n tau-vulnerability.simg docker://eturkes/r-template:R3.6.2v1
 
 if [ "$1" = "all" ]; then
     singularity exec \
-        -B .:/home/rstudio/r-template \
+        -B .:/home/rstudio/R-template \
         r-template.simg \
-    Rscript -e "source('/home/rstudio/r-template/R/run_all.R')"
+    Rscript -e "source('/home/rstudio/R-template/R/run_all.R')"
 
 elif [ "$1" = "rstudio" ]; then
     # TODO: Point bind point to user's home.
     DISABLE_AUTH=true RSTUDIO_SESSION_TIMEOUT="0" \
     singularity exec \
-        -B .:/home/rstudio/r-template \
+        -B .:/home/rstudio/R-template \
         r-template.simg rserver --www-address=127.0.0.1 --www-port=$2
 fi
